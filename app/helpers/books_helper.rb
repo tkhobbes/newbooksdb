21ab0c1405d97684a480692b6f7de24e8471a9b9
Thomas Keller <tkhobbes@gmail.com>
2022-07-19 18:11:17 +0200

    Book Show Page and cover display

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -7,4 +7,20 @@ module BooksHelper
       "(" + book.original_title + ")"
     end
   end
+
+  def rating_stars(rating)
+    rating.times do
+      content_tag(
+        :svg,
+        xlmns: "http://www.w3.org/2000/svg",
+        class: "smallicon",
+          viewBox: "0 0 24 24",
+          fill: "currentColor") do
+            content_tag(
+              :path,
+              d: "M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+            )
+          end
+    end
+  end
 end
