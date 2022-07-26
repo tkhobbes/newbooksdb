1aef40f5ead27e02e7d3e42f2a109c0ee9f09749
Thomas Keller <tkhobbes@gmail.com>
2022-07-26 14:53:50 +0200

    form styling and form errors

--- spec/views/books/index.html.erb_spec.rb
+++ spec/views/books/index.html.erb_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe "books/index.html.erb", type: :view do
 
   before(:all) do
-    @books = Array.new(3) { create(:random_book) }
+    @pagy, @books = pagy(Array.new(3) { create(:random_book) })
   end
 
   context 'Index page' do
     it 'displays a book card' do
       assign(:books, @books)
+      assign(:pagy, @pagy)
       render
       expect(rendered).to include('bookcard-big')
     end
