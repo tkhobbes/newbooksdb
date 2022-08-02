79c1539d1caea024665ef27e53df0c7ed4e54d3a
Thomas Keller <tkhobbes@gmail.com>
2022-08-02 10:05:02 +0200

    adding more cops and refactored a bit more

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -17,12 +17,12 @@ module BooksHelper
   # rubocop:disable Metrics/MethodLength
   def cover_image(book, options = {})
     cover_map = {
-      "large": [500, 'placeholder-large'],
-      "medium": [300, 'placeholder-medium'],
-      "small": [150, 'placeholder-small'],
-      "default": [50, 'placeholder-default']
+      large: [500, 'placeholder-large'],
+      medium: [300, 'placeholder-medium'],
+      small: [150, 'placeholder-small'],
+      default: [50, 'placeholder-default']
     }
-    size = options[:size]&.to_sym || 'default'
+    size = options[:size]&.to_sym || :default
     # below the duplicate method call
     img_size = cover_map[size][0]
     css_tag = cover_map[size][1]
@@ -45,7 +45,7 @@ module BooksHelper
         fill: 'currentColor',
         stroke: 'currentColor',
         # rubocop:disable Layout/LineLength
-        "stroke-width": '1') do
+        'stroke-width': '1') do
           tag.path(d: 'M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z')
         # rubocop:enable Layout/LineLength
       end * rating <<
@@ -56,10 +56,10 @@ module BooksHelper
         fill: 'none',
         viewBox: '0 0 24 24',
         stroke: 'currentColor',
-        "stroke-width": '1') do
+        'stroke-width': '1') do
           # rubocop:disable Layout/LineLength
-          tag.path("stroke-linecap": 'round',
-            "stroke-linejoin": 'round',
+          tag.path('stroke-linecap': 'round',
+            'stroke-linejoin': 'round',
             d: 'M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z')
             # rubocop:enable Layout/LineLength
         end * (5- rating)
@@ -96,11 +96,11 @@ module BooksHelper
       viewBox: '0 0 24 24',
       fill: 'none',
       stroke: 'currentColor',
-      "stroke-width": '2'
+      'stroke-width': '2'
     ) do
       # rubocop:disable Layout/LineLength
-      tag.path("stroke-linecap": 'round',
-        "stroke-linejoin": 'round',
+      tag.path('stroke-linecap': 'round',
+        'stroke-linejoin': 'round',
         d: 'M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253')
         # rubocop:enable Layout/LineLength
     end
