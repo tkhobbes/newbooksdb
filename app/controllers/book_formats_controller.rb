6e348ec92af17896a513dd017eb9b28da5b7f234
Thomas Keller <tkhobbes@gmail.com>
2022-08-03 15:49:09 +0200

    inline form for book-format created

--- app/controllers/book_formats_controller.rb
+++ app/controllers/book_formats_controller.rb
@@ -1,18 +1,35 @@
 # frozen_string_literal: true
 
 class BookFormatsController < ApplicationController
+  before_action :set_book_format, only: [:show, :edit, :update, :destroy]
+
+  #index action - displayed in a turbo frame within the settings page
   def index
     @formats = BookFormat.all.order(:format)
   end
 
-  def new; end
+  #new action - displayed in a turbo frame within the settings page
+  def new
+    @format = BookFormat.new
+  end
 
+  #standard rails create action
   def create; end
 
+  #edit action - displayed in a turbo frame within the settings page
   def edit; end
 
+  #standard rails update action
   def update; end
 
+  #standard rails destroy action
   def destroy; end
+
+  private
+
+  # retrieve a book format from the database
+  def set_book_format
+    @format = BookFormat.find(params[:id])
+  end
 
 end
