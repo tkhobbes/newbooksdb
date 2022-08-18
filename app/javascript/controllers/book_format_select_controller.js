fe53e9a48df146caa949e23cedf51de00691b340
Thomas Keller <tkhobbes@gmail.com>
2022-08-18 15:46:26 +0200

    add tom-select for nice forms

--- app/javascript/controllers/book_format_select_controller.js
+++ app/javascript/controllers/book_format_select_controller.js
@@ -1,8 +1,44 @@
 import { Controller } from "@hotwired/stimulus";
+import TomSelect from "tom-select";
 
 // Connects to data-controller="book-format-select"
 export default class extends Controller {
   connect() {
-    console.log("wave");
+    const selectInput = document.getElementById("book_book_format_id");
+    if (selectInput) {
+      new TomSelect(selectInput, {
+        plugins: {
+          remove_button: {
+            title: "Remove this item",
+          },
+        },
+        onItemAdd: function () {
+          this.setTextboxValue("");
+          this.refreshOptions();
+        },
+        persist: false,
+        create: function (input, callback) {
+          const data = { name: input };
+          const token = document.querySelector(
+            'meta[name="csrf-token"]'
+          ).content;
+          fetch("/book_formats", {
+            method: "POST",
+            headers: {
+              "X-CSRF-Token": token,
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: JSON.stringify(data),
+          })
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              callback({ value: data.id, text: data.name });
+            });
+        },
+      });
+    }
   }
 }
