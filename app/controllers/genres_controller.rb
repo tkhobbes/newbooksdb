82060f00560e8f6f44875612b0171f680d99bad2
Thomas Keller <tkhobbes@gmail.com>
2022-09-20 17:40:52 +0200

    genres can pagy and load more

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,22 +2,30 @@
 
 # standard Rails controller for the books model
 class GenresController < ApplicationController
+  # allow for turbo frame variants
+  before_action :turbo_frame_request_variant
 
   # list all genres and the first 5 books within each genre
   def index
-    @genres = Genre
+    @pagy, @genres = pagy(Genre
       .includes([:books_genres, books: [:user, cover_attachment: :blob]])
-      .order(:name)
+      .order(:name))
   end
 
   # shows a genre in detail and lists all books within that genre
   def show
-    @genre = Genre.includes([books: [:user, cover_attachment: :blob]]).find(params[:id])
+    @genre = Genre.find(params[:id])
+    @pagy, @books = pagy(@genre.books.includes([:user, cover_attachment: :blob]))
   end
 
   private
 
   def set_genre
     @genre = Genre.find(params[:id])
+  end
+
+  # enable turbo frame variants
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
   end
 end
