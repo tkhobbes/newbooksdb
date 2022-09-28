bb3e129f534ef2482adde4aca818508077ca7bb8
Thomas Keller <tkhobbes@gmail.com>
2022-09-28 14:44:27 +0200

    CRUD actions for shelves

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -3,6 +3,11 @@
 # standard rails controller for shelves; these actions are all only used within the
 # settings menu
 class ShelvesController < ApplicationController
+  # allow for turbo frame variants
+  before_action :turbo_frame_request_variant
+
+  before_action :set_shelf, only: [:show, :edit, :update, :destroy]
+
   # we need the session helper and the user concerns to ensure only logged in users can tamper with formats
   include SessionsHelper
   include UserConcerns
@@ -21,24 +26,62 @@ class ShelvesController < ApplicationController
     end
   end
 
-  def edit
-
-  end
+  # standard rails method to edit a shelf
+  def edit; end
 
   def update
-
+    if @shelf.update(shelf_params)
+      redirect_to shelves_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
+  # Standard rails new action
   def new
-
+    @shelf = Shelf.new
   end
 
+  #standard rails create action; answers to:
+  # -normal html (fallback and not used)
+  # -turbo stream - default response format, used on the settings page
+  # -json - used as we can create formats on the fly in the book form
+  # This method smells of :reek:DuplicateMethodCall
+  # This method smells of :reek:TooManyStatements
   def create
+    @shelf = Shelf.new(shelf_params)
+    @shelf.user_id = current_user.id
 
+    respond_to do |format|
+      if @shelf.save
+        format.turbo_stream
+        format.html { redirect_to shelves_path }
+        format.json { render json: @shelf }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+
+    end
   end
 
-  def destroy
 
+  #standard rails destroy action - responds to
+  # -html (not used)
+  # -turbo-stream - default response format, used on the settings page
+  # This method smells of :reek:TooManyStatements
+  def destroy
+   @shelf.destroy
+    if @shelf.destroyed?
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to shelves, status: :see_other }
+      end
+    else
+      # in the rare occasion where the format is not deleted -
+      # as it is the default - ensure that the corresponding
+      # turbo stream is not removed
+      render json: { nothing: true }
+    end
   end
 
   private
@@ -52,5 +95,15 @@ class ShelvesController < ApplicationController
   # rails strong params
   def shelf_params
     params.require(:shelf).permit(:name)
+  end
+
+  # allow turbo frames
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
+  end
+
+  # set shelf consistently
+   def set_shelf
+    @shelf = Shelf.find(params[:id])
   end
 end
