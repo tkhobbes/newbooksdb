fd57a97c7eb0a91000083ff8abd35b64d2626d66
Thomas Keller <tkhobbes@gmail.com>
2022-10-01 09:13:17 +0200

    publisher create new

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -5,11 +5,38 @@ class PublishersController < ApplicationController
   # allow for turbo frame variants
   before_action :turbo_frame_request_variant
 
+  # we need the session helper and the user concerns to ensure only logged in users can tamper with formats
+  include SessionsHelper
+  include UserConcerns
+
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+
+  before_action :set_publisher, only: [:show, :edit, :update, :destroy]
+
+  before_action :dissolve, only: [:show]
+
   # index method uses pagy
   def index
     @pagy, @publishers = pagy(Publisher.all
       .includes([books: [cover_attachment: :blob]])
       .order(:name))
+  end
+
+  # new method to display form
+  def new
+    @publisher = Publisher.new
+  end
+
+  # creation / storage of a new publisher
+  def create
+    @publisher = Publisher.new(publisher_params)
+
+    if @publisher.save
+      flash[:success] = 'Publisher saved'
+      redirect_to publisher_path(@publisher)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
