a70f30a8354e4396c1c7219f93c0f90e87edf628
Thomas Keller <tkhobbes@gmail.com>
2022-10-01 09:06:38 +0200

    button styles and new publisher button

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -1,7 +1,11 @@
 #frozen_string_literal: true
 
+# Standard Rails model for Publishers
 class PublishersController < ApplicationController
+  # allow for turbo frame variants
+  before_action :turbo_frame_request_variant
 
+  # index method uses pagy
   def index
     @pagy, @publishers = pagy(Publisher.all
       .includes([books: [cover_attachment: :blob]])
@@ -10,10 +14,12 @@ class PublishersController < ApplicationController
 
   private
 
+  # Rails strong params
   def publisher_params
     params.require(:publisher).permit(:name, :location)
   end
 
+  # standard method to get the publisher using friendly ID
   def set_publisher
     @publisher = Publisher.friendly.find(params[:id])
   end
