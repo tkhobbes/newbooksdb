e6167f779d845e7f1bf12f790e49a15dd46a8872
Thomas Keller <tkhobbes@gmail.com>
2022-10-04 09:29:43 +0200

    books shown in author show page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 class AuthorsController < ApplicationController
+  # allow for turbo frame variants
+  before_action :turbo_frame_request_variant
 
   before_action :set_author, only: %i[show edit update destroy]
+
+  before_action :dissolve, only: :index
 
   def index
     @authors = Author.all.order(:sort_name)
   end
 
-  def show; end
+  def show
+      @pagy, @books = pagy(@author.books.includes([:user, cover_attachment: :blob]).order(:sort_title))
+  end
 
   private
 
