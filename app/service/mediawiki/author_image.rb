5f4b4f641fe0e13034d2e5d431fbe2a6f3f0837b
Thomas Keller <tkhobbes@gmail.com>
2022-12-05 11:39:15 +0100

    refactored author image to separate namespace

--- app/service/mediawiki/author_image.rb
+++ app/service/mediawiki/author_image.rb
@@ -2,9 +2,32 @@ module Mediawiki
 
   # this service object retrieves the URL of an author from Mediawiki
   class AuthorImage
-    def get_author_picture(author)
 
+    def wiki_picture(author)
+      retrieve_picture_for(author)
     end
 
+    private
+
+    def retrieve_picture_for(author)
+      url = "https://www.mediawiki.org/w/api.php?action=query&titles=#{image_file_name(author)}&prop=imageinfo&iiprop=url&format=json"
+      json_doc = JSON.parse(Faraday.get(url).body, symbolize_names: true)
+      return unless json_doc
+      urls = json_doc.dig(:query, :pages, :"-1", :imageinfo)
+      return unless urls
+      urls.first[:url]
+    end
+
+    def image_file_name(author)
+      all_images = search_author_in_wiki(author)
+      if all_images
+        all_images.select { |i| i[-3,3] == 'jpg' || i[-3,3] == 'png' }.first&.gsub(' ', '%20')
+      end
+    end
+
+    def search_author_in_wiki(author)
+      wiki = MediaWiki::Butt.new('https://en.wikipedia.org/w/api.php')
+      wiki.get_images_in_page(author) # returns nil if page is not found
+    end
   end
 end
