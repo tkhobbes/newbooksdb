2d8ed74c7aa80529a97f27a7d1755e7640da6759
Thomas Keller <tkhobbes@gmail.com>
2023-01-11 13:24:03 +0100

    Fixes for flash messages

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -17,7 +17,7 @@ class ProfilesController < ApplicationController
 
   def update
     if @profile.update(profile_params)
-      redirect_to(profile_path(@profile))
+      redirect_to profile_path(@profile), success: 'Profile updated'
     else
       render :edit, status: :unprocessable_entity
     end
