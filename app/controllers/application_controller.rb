6a051fd58e1ab49bb2f946b6b3a38f542d94ba8d
Thomas Keller <tkhobbes@gmail.com>
2023-01-31 16:56:15 +0100

    views can handle >1 author

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,16 +16,16 @@ class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :success
 
   # Prosopite enablement
-  unless Rails.env.production?
-    around_action :n_plus_one_detection
+  # unless Rails.env.production?
+  #   around_action :n_plus_one_detection
 
-    def n_plus_one_detection
-      Prosopite.scan
-      yield
-    ensure
-      Prosopite.finish
-    end
-  end
+  #   def n_plus_one_detection
+  #     Prosopite.scan
+  #     yield
+  #   ensure
+  #     Prosopite.finish
+  #   end
+  # end
 
   # enable turbo frame variants
   def turbo_frame_request_variant
