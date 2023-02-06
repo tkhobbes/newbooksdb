f3dba66dc9be7f4c1a82b927ec09fabec73c7e3f
Thomas Keller <tkhobbes@gmail.com>
2023-02-06 13:53:28 +0100

    Prepare for docker deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => Rails.application.credentials.gmail[:user],
-    :password             => Rails.application.credentials.gmail[:password],
+    :user_name            => Rails.application.credentials.gmail.user,
+    :password             => Rails.application.credentials.gmail.password,
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
