b0bb1eb9f0b51cc6e851a9c1a8425673a2c6450b
Thomas Keller <tkhobbes@gmail.com>
2023-02-20 14:40:19 +0100

    AuthorPortrait ViewComponent

---
+++ app/components/author_portrait_component.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class AuthorPortraitComponent < ViewComponent::Base
+  # returns either the portrait of an author or a placeholder SVG
+
+  PORTRAIT_SIZES = {
+    large: [500, 'placeholder-large'],
+    medium: [300, 'placeholder-medium'],
+    small: [150, 'placeholder-small'],
+    default: [50, 'placeholder-default']
+  }
+
+  def initialize(author:, size: :default)
+    @author = author
+    @size = size
+    super
+  end
+
+  def portrait_image
+    img_size = PORTRAIT_SIZES[@size][0]
+    css_tag = PORTRAIT_SIZES[@size][1]
+
+    portrait = @author.portrait
+    if portrait.attached?
+      image_tag(portrait.variant(resize_to_limit: [img_size, img_size]), size: img_size)
+    else
+      generate_author_portrait_svg(css_tag, "#{img_size}px")
+    end
+  end
+    private
+
+  # generates a SVG tag with the given class
+  def generate_author_portrait_svg(css, size)
+    inline_svg_tag('person.svg', class: css, size:)
+  end
+
+end
--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -5,36 +5,4 @@ module AuthorsHelper
   def authors_count
     Rails.cache.fetch('authors-count') { Author.count }
   end
-
-  # returns either the portrait of an author or a placeholder SVG
-  # This method smells of :reek:DuplicateMethodCall
-  # This method smells of :reek:TooManyStatements
-  # rubocop:disable Metrics/MethodLength
-  def portrait_image(author, options = {})
-    portrait_map = {
-      large: [500, 'placeholder-large'],
-      medium: [300, 'placeholder-medium'],
-      small: [150, 'placeholder-small'],
-      default: [50, 'placeholder-default']
-    }
-    size = options[:size]&.to_sym || :default
-    # below the duplicate method call
-    img_size = portrait_map[size][0]
-    css_tag = portrait_map[size][1]
-
-    portrait = author.portrait
-    if portrait.attached?
-      image_tag(portrait.variant(resize_to_limit: [img_size, img_size]), size: img_size)
-    else
-      generate_author_portrait_svg(css_tag, "#{img_size}px")
-    end
-  end
-  # rubocop:enable Metrics/MethodLength
-
-  private
-
-  # generates a SVG tag with the given class
-  def generate_author_portrait_svg(css, size)
-    inline_svg_tag('person.svg', class: css, size:)
-  end
 end
