5785f903ca7c23cb77b532f2cad7cb875bc0cf66
Thomas Keller <tkhobbes@gmail.com>
2023-03-16 09:41:42 +0100

    empty model spec files

---
+++ spec/models/books_genre_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# rubocop:disable Metrics/BlockLength
+RSpec.describe BooksGenre, type: :model do
+
+end
+# rubocop:enable Metrics/BlockLength
---
+++ spec/models/profile_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# rubocop:disable Metrics/BlockLength
+RSpec.describe Profile, type: :model do
+
+end
+# rubocop:enable Metrics/BlockLength
