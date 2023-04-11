da1e7747b0c9dc90537fae01c2ce1868e5dea865
Thomas Keller <tkhobbes@gmail.com>
2023-04-11 11:32:04 +0200

    first request spec (author)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,9 @@ RSpec.configure do |config|
   # for factory bot, commented out above line and added below:
   config.include FactoryBot::Syntax::Methods
 
+  # Devise helpers
+  config.include Devise::Test::IntegrationHelpers, type: :request
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
---
+++ spec/requests/authors_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Authors' do
+  describe 'authentication' do
+    context 'not logged in' do
+      it 'can see authors index without login' do
+        get authors_path
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'can see authors show without login' do
+        author = create(:author)
+        get author_path(author.id)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'must log in' do
+      it 'cannot create an author without being logged in' do
+        post authors_path, params: {
+          author: {
+            first_name: 'JRR',
+            last_name: 'Tolkien'
+          }
+        }
+        expect(Author.count).to eq(0)
+      end
+
+      it 'cannot update an author without being logged in' do
+        author = create(:author)
+        patch author_path(author.id), params: {
+          author: {
+            first_name: 'new'
+          }
+        }
+        expect(author.reload.first_name).to eq('John')
+      end
+
+      it 'cannot delete an author without being logged in' do
+        author = create(:author)
+        delete author_path(author.id)
+        expect(Author.count).to eq(1)
+      end
+    end
+
+    context 'has logged in' do
+      let(:owner) { create(:owner) }
+
+      it 'can create an author when logged in' do
+        sign_in owner
+        post authors_path, params: {
+          author: {
+            first_name: 'JRR',
+            last_name: 'Tolkien'
+          }
+        }
+        expect(Author.count).to eq(1)
+      end
+
+      it 'can update an author when logged in' do
+        author = create(:author)
+        sign_in owner
+        patch author_path(author.id), params: {
+          author: {
+            first_name: 'new'
+          }
+        }
+        expect(author.reload.first_name).to eq('new')
+      end
+
+      it 'can delete an author when logged in' do
+        author = create(:author)
+        sign_in owner
+        delete author_path(author.id)
+        expect(Author.count).to eq(0)
+      end
+    end
+  end
+end
