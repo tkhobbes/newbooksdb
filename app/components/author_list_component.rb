402ae790756bed2e07f32cb02549d49ad11ad174
Thomas Keller <tkhobbes@gmail.com>
2023-05-02 13:06:37 +0200

    locale bugfixing

--- app/components/author_list_component.rb
+++ app/components/author_list_component.rb
@@ -34,7 +34,7 @@ class AuthorListComponent < ViewComponent::Base
   # shows (by ...) for the first author and adds "et al." if there are more than one
   def by_authors
     return if @authors.blank?
-    "(#{t('AuthorListComponent.by', author: @authors.first.display_name)} \
+    "(#{t('AuthorListComponent.by', author: @authors.first.display_name)}\
     #{@authors.count > 1 ? t('AuthorListComponent.more') : ''})"
   end
 
